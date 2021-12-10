e86a7b2f70c3d10ae4334ff57945f4d6f05b5e62
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2021-12-10 16:37:00 -0700

    updates build

---
+++ MochaMarks/client/assets/AboutPage.5d3e2b30.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.327130aa.js";import{o,b as t,d as a}from"./vendor.fa40ae07.js";const s={name:"AboutPage"},n={class:"about"},r=a("h1",null,"This is the about page",-1),c=[r];function _(d,i,p,u,f,h){return o(),t("div",n,c)}var b=e(s,[["render",_]]);export{b as default};
