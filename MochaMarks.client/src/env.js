a47b7539fd9b4a1d34404de0fcb705570e6fe49b
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2021-12-10 16:12:26 -0700

    init

---
+++ MochaMarks.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-km1-vdq2.us.auth0.com'
+export const clientId = '803qRL6tNlLSYhhqqVWc2iSNy65YgPhi'
+export const audience = 'https://mark-dev.com'
